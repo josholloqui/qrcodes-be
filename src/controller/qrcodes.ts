6bd1fc311700de945dc5af5412ecaefdeac1e6d4
Josh <josh.olloqui@gmail.com>
2020-11-08 14:42:10 -0800

    added and tested getbyid updatebyid and delete

--- src/controller/qrcodes.ts
+++ src/controller/qrcodes.ts
@@ -3,24 +3,59 @@ import QrCode from '../models/qrcode';
 
 export const qrCodes = Router();
 
-qrCodes.post('/', async (req, res, next) => {
-  try {
-    const createQrCode = await QrCode.create({
-      title: req.body.title,
-      url: req.body.url,
-      qr_code: `https://qrtag.net/api/qr_5.png?url=${req.body.url}`
+qrCodes
+  .post('/', async (req, res, next) => {
+    try {
+      const createQrCode = await QrCode.create({
+        title: req.body.title,
+        url: req.body.url,
+        qr_code: `https://qrtag.net/api/qr_5.png?url=${req.body.url}`
+      });
+      res.status(201).json(createQrCode)
+    } catch (e) {
+      next(e);
+    }
+  })
+  .get('/', async (_req, res, next) => {
+    try {
+      const getAllQrCodes = await QrCode.findAll();
+      res.status(200).json(getAllQrCodes);
+    } catch (e) {
+      next(e);
+    }
+  })
+  .get('/:id', async (req, res, next) => {
+    try {
+      const getQrCode = await QrCode.findByPk(req.params.id);
+      res.status(200).json(getQrCode)
+    } catch (e) {
+      next(e);
+    }
+  })
+  .put('/:id', async (req, res, next) => {
+    try {
+      const updateQrCode = await QrCode.update(
+        {
+          title: req.body.title,
+          url: req.body.url
+        },
+        {
+          where: {
+            id: req.params.id
+          }
+        }
+      );
+      res.status(202).json(updateQrCode)
+    } catch (e) {
+      next(e);
+    }
+  })
+  .delete('/:id', async (req, res, next) => {
+    const deleteQrCode = await QrCode.destroy({
+      where: {
+        id: req.params.id
+      }
     });
-    res.status(201).json(createQrCode)
-  } catch (e) {
-    next(e);
-  }
-});
+    res.status(202).json(deleteQrCode)
+  })
 
-qrCodes.get('/', async (_req, res, next) => {
-  try {
-    const getAllQrCodes = await QrCode.findAll();
-    res.status(201).json(getAllQrCodes);
-  } catch (e) {
-    next(e);
-  }
-})
--- test/qrCodes.test.ts
+++ test/qrCodes.test.ts
@@ -4,7 +4,7 @@ import { sequelize } from '../src/sequelize';
 
 describe('POST to qrcodes', () => {
   beforeEach(async () => { await sequelize.sync({ force: true }); })
-  it('should return 200 OK', async () => {
+  it('should return 201 CREATED', async () => {
     const response = await request(app).post('/qrcodes')
       .send({
         title: 'Twitter',
@@ -36,5 +36,57 @@ describe('POST to qrcodes', () => {
           expect(res.body).toContainEqual(qrcode.body);
         })
       })
-  })
+  });
+
+  it('should find qrcode by id', async () => {
+    const newQrCode = await request(app).post('/qrcodes')
+      .send({
+        title: 'Twitter',
+        url: 'https://twitter.com'
+      });
+
+    const foundQrCode = await request(app)
+      .get(`/qrcodes/${newQrCode.body.id}`)
+    expect(foundQrCode.body).toEqual(newQrCode.body);
+  });
+
+  it('should update a qrcode by its id', async () => {
+    const newQrCode = await request(app).post('/qrcodes')
+      .send({
+        title: 'Twitter',
+        url: 'https://twitter.com'
+      });
+
+    await request(app)
+      .put(`/qrcodes/${newQrCode.body.id}`)
+      .send({
+        title: 'Twitter3',
+        url: 'https://twitter.com'
+      });
+
+    return request(app)
+      .get(`/qrcodes/${newQrCode.body.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(Number),
+          title: 'Twitter3',
+          url: 'https://twitter.com',
+          qr_code: expect.any(String),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String)
+        })
+      })
+  });
+
+  it('should return 200 OK', async () => {
+    const createQrCode = await request(app).post('/qrcodes')
+      .send({
+        title: 'Twitter',
+        url: 'https://twitter.com'
+      });
+
+    const response = await request(app)
+      .delete(`/qrcodes/${createQrCode.body.id}`)
+      .expect(202);
+  });
 });
